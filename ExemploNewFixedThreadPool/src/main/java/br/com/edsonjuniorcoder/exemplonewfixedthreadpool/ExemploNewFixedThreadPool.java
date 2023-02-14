6728438b06947d0ef0b289c0faee4a55b937dcd6
edjuniorcoder <edsonjuniorcoder@gmail.com>
2023-02-13 21:31:28 -0300

    Adicionado arquivo pom.xml e classe ExemploNewFixedThreadPool.java

---
+++ ExemploNewFixedThreadPool/src/main/java/br/com/edsonjuniorcoder/exemplonewfixedthreadpool/ExemploNewFixedThreadPool.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.edsonjuniorcoder.exemplonewfixedthreadpool;
+
+/**
+ *
+ * @author jerry
+ */
+public class ExemploNewFixedThreadPool {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
